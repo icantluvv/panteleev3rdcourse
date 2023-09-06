9fd4fae10c0c81f89803d1072b9074d6a6fd33bc
Sergey <spanteleev670@gmail.com>
2023-09-06 10:54:42 +0300

    hometask0509
    made a code for searching numbers multiple of 3 or 5 from 0 to N

---
+++ hometask1.py
@@ -0,0 +1,10 @@
+def task(n):
+    n -= 1
+    sum_3 = 3 * (n // 3 * (n // 3 + 1) // 2)
+    sum_5 = 5 * (n // 5 * (n // 5 + 1) // 2)
+    sum_15 = 15 * (n // 15 * (n // 15 + 1) // 2)
+    return sum_3 + sum_5 - sum_15
+
+n = int(input("Введите n: "))
+result = task(n)
+print(f"Сумма чисел от 1 до {n}, делящихся на 3 или 5: {result}")
