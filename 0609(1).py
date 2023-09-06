422438899a233230a9d44f27ced222413aa3b2ae
Sergey <spanteleev670@gmail.com>
2023-09-06 12:13:43 +0300

    Update 0609(1).py

--- 0609(1).py
+++ 0609(1).py
@@ -1,9 +1,23 @@
-numbers: list = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9
-]
+# numbers: list = [
+#     1, 2, 3, 4, 5, 6, 7, 8, 9
+# ]
 
-for n in numbers:
-    if n % 3 == 0:
-            print(f'число кратно 3 - {n}')
-    else:
-        print(f'число не кратно 3 - {n}')
+# for n in numbers:
+#     if n % 3 == 0:
+#             print(f'число кратно 3 - {n}')
+#     else:
+#         print(f'число не кратно 3 - {n}')
+
+
+n = int(input())
+
+# while n > 0:
+#     print(n)
+#     n -= 1
+
+a = []
+while n > 0:
+    a.append(n)
+    n -= 1
+a.reverse()
+print(a)    
